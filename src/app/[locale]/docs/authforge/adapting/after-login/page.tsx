6b90896cac3987f98c9522cc370b6a6fa2b26c2b
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2026-01-31 17:30:31 +0300

    chore(submodule): update authforge documentation

--- src/app/[locale]/docs/authforge/adapting/after-login/page.tsx
+++
@@ -1,5 +0,0 @@
-import DocPage from '../../_components/DocPage';
-
-export default function AfterLoginPage() {
-  return <DocPage slug={['adapting', 'after-login']} />;
-}
--- src/app/[locale]/docs/authforge/adapting/email/page.tsx
+++
@@ -1,5 +0,0 @@
-import DocPage from '../../_components/DocPage';
-
-export default function EmailAdaptingPage() {
-  return <DocPage slug={['adapting', 'email']} />;
-}
